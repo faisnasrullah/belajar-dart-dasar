83e1f5309ac8e6f8b15532918b6f5a6c33a57d41
Fais Nasrullah <fais@automatalabs.co>
2021-12-18 06:53:50 +0700

    Operator Perbandingan pada Dart

---
+++ bin/4_operator_perbandingan.dart
@@ -0,0 +1,19 @@
+/// dokumentasi belajar operator perbandingan di dart
+void main() {
+  /**
+   * '==' -> Sama dengan
+   * '!=' -> Tidak sama dengan
+   * '>' -> Lebih dari
+   * '<' -> Kurang dari
+   * '>=' -> Lebih dari atau sama dengan
+   * '<=' -> Kurang dari atau sama dengan
+   */
+
+  print(10 == 10);
+  print(10 != 10);
+  print(10 > 10);
+  print(10 < 10);
+  print(10 >= 10);
+  print(10 <= 10);
+  print('fais' == 'nasrullah');
+}
