0fe40c89d363cadeb2ad88faf54e963669344584
Fais Nasrullah <fais@automatalabs.co>
2021-12-21 17:59:34 +0700

    Symbol pada Dart

---
+++ bin/11_symbol.dart
@@ -0,0 +1,14 @@
+/// dokumentasi belajar tipe data symbol di dart
+void main() {
+  /**
+   * symbol bisa digunakan sebagai constant
+   * kita bisa menggunakan tanda #
+   * atau bisa menggunakan Symbol('text') jika lebih dari satu kata
+   */
+
+  Symbol symbol1 = Symbol('Fais Nasrullah');
+  var symbol2 = #belajar_dart;
+
+  print(symbol1);
+  print(symbol2);
+}
