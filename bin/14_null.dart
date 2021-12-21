0ef7e222615eb16d711438ff04bba6b57f9de99d
Fais Nasrullah <fais@automatalabs.co>
2021-12-21 18:50:44 +0700

    Null di dart

---
+++ bin/14_null.dart
@@ -0,0 +1,17 @@
+/// dokumentasi belajar switch di dart
+void main() {
+  /**
+   * null == data kosong
+   * digunakan jika kita ingin membuat sebuah variabel yg bisa
+   * diisi dengan data null, kita bisa tambahkan karakter ?
+   * ex :
+   * TipeData? namaVariabel = null;
+   * 
+   */
+
+  int number1;
+  int? number2;
+
+  print(number1);
+  print(number2);
+}
