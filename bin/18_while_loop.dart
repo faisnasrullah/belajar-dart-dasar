e1c2f442ed0432086dabda91ed82b4ff78556536
Fais Nasrullah <fais@automatalabs.co>
2022-01-04 17:38:04 +0700

    While Loop pada Dart

---
+++ bin/18_while_loop.dart
@@ -0,0 +1,13 @@
+/// dokumentasi belajar while loop di dart
+void main() {
+  /**
+   * while loop adalah versi perulangan yg lebih sederhana
+   * hanya terdapat kondisi perulangan tanpa ada init dan post state
+   */
+
+  var counter = 1;
+  while (counter <= 10) {
+    print('Perulangan ke-$counter');
+    counter++;
+  }
+}
