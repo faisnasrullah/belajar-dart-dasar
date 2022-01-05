80762e6c4cc978add9602a51c46f298bd67185b1
Fais Nasrullah <fais@automatalabs.co>
2022-01-05 18:16:00 +0700

    For in pada Dart

---
+++ bin/21_for_in.dart
@@ -0,0 +1,13 @@
+/// dokumentasi belajar for in di dart
+void main() {
+  /**
+   * mengakses data list menggunakan perulangan sangat bertele-tele
+   * cara yg lebih cepat adalah menggunakan for in
+   */
+
+  var array = <String>['Eko', 'Fais', 'Farid', 'Ronal', 'Arif'];
+
+  for (var value in array) {
+    print(value);
+  }
+}
