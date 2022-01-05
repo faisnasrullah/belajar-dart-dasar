daf089eb2ecbef545dfb21ed7d17a5e404ced7af
Fais Nasrullah <fais@automatalabs.co>
2022-01-05 18:17:28 +0700

    Function pada Dart

---
+++ bin/22_function.dart
@@ -0,0 +1,16 @@
+/// dokumentasi belajar function di dart
+
+// simple function
+void sayHello() {
+  print('Hello World');
+}
+
+void main() {
+  /**
+   * 
+   */
+
+  // panggil function sayHello()
+  sayHello();
+  sayHello();
+}
