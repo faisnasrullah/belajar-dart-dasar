e9e429f5b2ae5a7fef142b96b68dc1cf640130c9
Fais Nasrullah <fais@automatalabs.co>
2022-01-05 19:15:30 +0700

    Function Short Expression

---
+++ bin/27_function_short_expression.dart
@@ -0,0 +1,14 @@
+/// dokumentasi belajar function short expression di dart
+
+// simple function
+int sum(int first, int second) => first + second;
+
+void main() {
+  /**
+   * function yang hanya satu baris
+   * kita bisa menggunakan => dan tanpa {} dan keyword return
+   */
+
+  print(sum(10, 55));
+  print(sum(87, 15));
+}
