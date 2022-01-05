19aadc54af1ce94076e72fd69012271337a412f4
Fais Nasrullah <fais@automatalabs.co>
2022-01-05 18:22:23 +0700

    Function with Parameter pada Dart

---
+++ bin/23_function_parameter.dart
@@ -0,0 +1,16 @@
+/// dokumentasi belajar function parameter di dart
+
+// simple function
+void sayFullName(String firstName, String lastName) {
+  print('Hai $firstName $lastName');
+}
+
+void main() {
+  /**
+   * 
+   */
+
+  // panggil function sayFullName()
+  sayFullName('Fais', 'Nasrullah');
+  sayFullName('Siswi Sekar', 'Sari');
+}
